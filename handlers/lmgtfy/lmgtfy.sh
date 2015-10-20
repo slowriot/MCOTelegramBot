4984bdd84831370cf69ba9c9a5399a755e11cb2d
Pedro Cunha <pedroagracio@gmail.com>
2015-10-20 18:11:27 +0100

    Improve wording if the query isn't specified

--- handlers/lmgtfy/lmgtfy.sh
+++ handlers/lmgtfy/lmgtfy.sh
@@ -5,7 +5,7 @@ scriptdir="$(dirname "${BASH_SOURCE[0]}")"
 args="$(cut -sd ' ' -f 2- <<< "$MCOBOT_TEXT")"
 
 if [ -z "$args" ]; then
-  echo "What do you want to search?"
+  echo "What do you want to search? Try again with /lmgtfy <query>"
 else
   escaped_args="$("$scriptdir"/../../urlencode.sh "$args")"
   echo "http://lmgtfy.com/?q=$escaped_args"
